5c2442430f3be78ad1efb79acde9a630e6c09142
Steve Ho <showrx@gmail.com>
2011-06-24 09:36:24 -0700

    first commit

--- AssetLib/AssetLibViewController.h
+++ AssetLib/AssetLibViewController.h
@@ -7,9 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
 
-@interface AssetLibViewController : UIViewController {
-    
+@interface AssetLibViewController : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+   IBOutlet UITableView *tableView;       
+   IBOutlet UIActivityIndicatorView *activity;
+   NSMutableArray *assets; 
 }
 
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView *activity;
+
 @end
+
+
+/*
+ 16 pictures (4 x 4 grid - centered)
+ 
+ iPhone: 480 x 320
+ Pictures 75  x 75, 150px for big pict
+ 4px margin all around
+ 
+ iPad: 1024 x 768
+ pictures 175 x 175, 240px for big pict
+ 12px margin 
+ */
--- AssetLib/AssetLibViewController.m
+++ AssetLib/AssetLibViewController.m
@@ -10,6 +10,9 @@
 
 @implementation AssetLibViewController
 
+@synthesize tableView;
+@synthesize activity;
+
 - (void)dealloc
 {
     [super dealloc];
@@ -25,13 +28,73 @@
 
 #pragma mark - View lifecycle
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [activity startAnimating];
+
+    
+    void (^assetEnumerator)(ALAsset *, NSUInteger, BOOL *) = ^(ALAsset *result, NSUInteger index, BOOL *stop)
+    {
+        if(result != nil)
+        {
+            [assets addObject:result];
+        }
+    };    
+    
+    void (^assetGroupEnumerator)(ALAssetsGroup *, BOOL *) = ^(ALAssetsGroup *group, BOOL *stop){
+        if(group != nil)
+        {
+            [group enumerateAssetsUsingBlock:assetEnumerator];
+            
+        }  
+        [self.tableView reloadData];
+        [self.activity stopAnimating];
+        [self.activity setHidden:YES];
+            
+    };
+    
+    
+    assets = [[NSMutableArray alloc] init];
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    [library enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:assetGroupEnumerator failureBlock: ^(NSError *error) { NSLog(@"Failure");}];
 }
-*/
+
+#pragma mark UITableViewDataSource Methods
+
+
+// Customize the number of sections in the table view.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [assets count];
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+	ALAsset *asset = [assets objectAtIndex:indexPath.row];
+	[cell.imageView setImage:[UIImage imageWithCGImage:[asset thumbnail]]];
+	[cell.textLabel setText:[NSString stringWithFormat:@"Photo %d", indexPath.row+1]];
+
+
+    return cell;
+}
+
 
 - (void)viewDidUnload
 {
